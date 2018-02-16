266c30a8351a343ffba392bae23526ba728cd4cf
CPerry26 <codyy@live.com>
2018-02-15 19:37:20 -0500

    Added auto_run logic to the constructor context. Next the logic for the actual auto run event test will be written.

--- spec/goby/event/event_spec.rb
+++ spec/goby/event/event_spec.rb
@@ -3,21 +3,24 @@ require 'goby'
 RSpec.describe Event do
   let(:event) { Event.new }
   let(:entity) { Entity.new }
-  
+
   context "constructor" do
     it "has the correct default parameters" do
       expect(event.command).to eq "event"
       expect(event.mode).to eq 0
       expect(event.visible).to eq true
+      expect(event.auto_run).to eq false
     end
 
     it "correctly assigns custom parameters" do
       box = Event.new(command: "open",
                       mode: 1,
-                      visible: false)
+                      visible: false,
+                      auto_run: true)
       expect(box.command).to eq "open"
       expect(box.mode).to eq 1
       expect(box.visible).to eq false
+      expect(box.auto_run).to eq true
     end
   end
 
@@ -26,6 +29,8 @@ RSpec.describe Event do
       entity = Entity.new
       expect { event.run(entity) }.to output(Event::DEFAULT_RUN_TEXT).to_stdout
     end
+
+    # Auto run test logic will be placed in this context.
   end
 
 end
