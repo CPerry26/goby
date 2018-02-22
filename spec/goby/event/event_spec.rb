ef8e940f7dd6f765ae5fc466a998ea30ef9a37f0
CPerry26 <codyy@live.com>
2018-02-21 22:25:26 -0500

    Accidentally placed test cases inside the wrong spec file. Removed them.

--- spec/goby/event/event_spec.rb
+++ spec/goby/event/event_spec.rb
@@ -9,18 +9,15 @@ RSpec.describe Event do
       expect(event.command).to eq "event"
       expect(event.mode).to eq 0
       expect(event.visible).to eq true
-      expect(event.auto_run).to eq false
     end
 
     it "correctly assigns custom parameters" do
       box = Event.new(command: "open",
                       mode: 1,
-                      visible: false,
-                      auto_run: true)
+                      visible: false)
       expect(box.command).to eq "open"
       expect(box.mode).to eq 1
       expect(box.visible).to eq false
-      expect(box.auto_run).to eq true
     end
   end
 
@@ -29,8 +26,5 @@ RSpec.describe Event do
       entity = Entity.new
       expect { event.run(entity) }.to output(Event::DEFAULT_RUN_TEXT).to_stdout
     end
-
-    # Auto run test logic will be placed in this context.
   end
-
 end
